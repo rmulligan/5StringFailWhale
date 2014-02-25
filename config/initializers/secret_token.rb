eae17ec65c52889392f52f9971344625e7dd01a3
Ryan Mulligan <mullymulligan@gmail.com>
2014-02-25 00:52:12 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FiveStringFailWhale::Application.config.secret_token = 'c93d298861dcbc697837af0f183d7982fa2d1da0e06bd0c2f3101832c01061340b50116e3c313a86f1543128dbf30e2affd564a698832e5687a07c4b617ee6b2'
