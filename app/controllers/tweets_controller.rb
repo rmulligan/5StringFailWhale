4bd0eabab6d69353677b844366bbed49236bd583
Ryan Mulligan <mullymulligan@gmail.com>
2014-02-26 19:24:46 -0800

    Created routes and twitter-search api for ajax call

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,3 +1,14 @@
 class TweetsController < ApplicationController
+  def index;end;
 
+  def search
+    latitude  = params[:latitude].to_f
+    longitude = params[:longitude].to_f
+    radius    = params[:radius].to_i
+    tag       = params[:tag]
+
+    relevent_tweets = Tweet.in(:tags=> [tag]).geo_near([ latitude, longitude ]).max_distance(radius)
+
+    render :json => relevent_tweets.to_json
+  end
 end
