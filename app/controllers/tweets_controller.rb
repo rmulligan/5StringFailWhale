5a4c50104d4f8a7591c45555bc605f8294472cec
Ryan Mulligan <mullymulligan@gmail.com>
2014-02-26 23:36:28 -0800

    Sort results by descending created_at field

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,9 +4,9 @@ class TweetsController < ApplicationController
   def search
     latitude  = params[:latitude].to_f
     longitude = params[:longitude].to_f
-    radius    = params[:radius].to_f * (Math::PI / 180)
+    radius    = params[:radius].to_f * (Math::PI / 180) # Convert to radians
     tags      = params[:tags].split(",").map{|s|s.strip.reverse.chomp("#").reverse}
 
-    @relevent_tweets = Tweet.in(:tags=> tags).geo_near([ latitude, longitude ]).max_distance(radius) #convert to radians
+    @relevent_tweets = Tweet.in(:tags=> tags).geo_near([ latitude, longitude ]).max_distance(radius).sort { |x,y| x.created_at <=> y.created_at }
   end
 end
