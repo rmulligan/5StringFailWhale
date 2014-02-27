7c82dc02417295149f8514beed199fa5fce2b0cc
Ryan Mulligan <mullymulligan@gmail.com>
2014-02-27 15:44:21 -0800

    Added tests for added fields

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Tweet do
   it { should have_fields(:loc, :tags).of_type(Array) }
+  it { should have_fields(:content, :image_url, :username).of_type(String) }
   it { should be_timestamped_document }
   it { should have_index_for({ loc: '2d', tags: 1}).with_options(background: true) }
 
