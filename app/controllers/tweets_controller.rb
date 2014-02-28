e5beb33fc610ee21cdb474076da9f7393b43ce4a
Ryan Mulligan <mullymulligan@gmail.com>
2014-02-27 20:12:49 -0800

    Query for both lowercase and capitalized hashtags

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,8 +5,8 @@ class TweetsController < ApplicationController
     latitude  = params[:latitude].to_f
     longitude = params[:longitude].to_f
     radius    = params[:radius].to_f * (Math::PI / 180) # Convert to radians
-    tags      = params[:tags].split(",").map{|s|s.strip.reverse.chomp("#").reverse}
-
-    @relevent_tweets = Tweet.in(:tags=> tags).geo_near([ latitude, longitude ]).max_distance(radius).sort { |x,y| x.created_at <=> y.created_at }
+    tags      = params[:tags].split(",").map{|s|s.strip.reverse.chomp("#").reverse.downcase}
+    tags << tags.map(&:titlecase)
+    @relevent_tweets = Tweet.in(:tags=> tags.flatten).geo_near([ latitude, longitude ]).max_distance(radius).sort { |x,y| x.created_at <=> y.created_at }
   end
 end
